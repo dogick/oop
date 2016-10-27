2bf7634599f215a6529bc1b801bd31fef65a5b66
dogick <vadim.dolganoff2014@gmail.com>
2016-10-27 17:50:07 +0300

    Add files via upload

--- lab3/Calculator/TEST/TEST.cpp
+++ lab3/Calculator/TEST/TEST.cpp
@@ -1,11 +1,55 @@
-// TEST.cpp: определяет точку входа для консольного приложения.
+// TrimBlanksTests.cpp : Defines the entry point for the console application.
 //
-
 #include "stdafx.h"
+#include "../Calculator/Calculator.h"
+#include <boost/function.hpp>
 
-
-int main()
+struct CCalculatorFixture
 {
-    return 0;
-}
+    CCalculator calculator;
+};
 
+BOOST_FIXTURE_TEST_SUITE(Calculator, CCalculatorFixture)
+    BOOST_AUTO_TEST_CASE(can_determine_the_identifier)
+    {
+        BOOST_CHECK(calculator.DefineVar("x") == ReturnCode::NO_ERRORS);
+    }
+
+    BOOST_AUTO_TEST_CASE(identifier_can_not_start_with_a_digit)
+    {
+        BOOST_CHECK(calculator.DefineVar("2size") == ReturnCode::INCORRECTLY_IDENTIFIER);
+    }
+
+    BOOST_AUTO_TEST_CASE(identifier_can_not_be_empty)
+    {
+        BOOST_CHECK(calculator.DefineVar("") == ReturnCode::INCORRECTLY_IDENTIFIER);
+    }
+
+    BOOST_AUTO_TEST_CASE(identifier_can_not_be_re_declared)
+    {
+        BOOST_CHECK(calculator.DefineVar("position") == ReturnCode::NO_ERRORS);
+        BOOST_CHECK(calculator.DefineVar("position") == ReturnCode::THIS_IDENTIFIER_ALREADY_DECLARED);
+    }
+
+    BOOST_AUTO_TEST_CASE(identifier_can_only_be_letters_of_the_english_alphabet)
+    {
+        BOOST_CHECK(calculator.DefineVar("позиция") == ReturnCode::INCORRECTLY_IDENTIFIER);
+        BOOST_CHECK(calculator.DefineVar("position") == ReturnCode::NO_ERRORS);
+    }
+
+    BOOST_AUTO_TEST_CASE(in_the_identifier_can_be_an_underscore)
+    {
+        BOOST_CHECK(calculator.DefineVar("rectangle_position") == ReturnCode::NO_ERRORS);
+    }
+
+    BOOST_AUTO_TEST_CASE(in_the_identifier_may_not_be_other_special_characters)
+    {
+        BOOST_CHECK(calculator.DefineVar("rectangle+position") == ReturnCode::INCORRECTLY_IDENTIFIER);
+    }
+
+/*  BOOST_AUTO_TEST_CASE(after_the_declaration_of_its_identifier_value_equal_to_nan)
+    {
+        BOOST_CHECK(calculator.DefineVar("позиция") == ReturnCode::INCORRECTLY_IDENTIFIER);
+        BOOST_CHECK(calculator.DefineVar("position") == ReturnCode::NO_ERRORS);
+    }*/
+BOOST_AUTO_TEST_SUITE_END()
--- lab3/Calculator/TEST/stdafx.cpp
+++ lab3/Calculator/TEST/stdafx.cpp
@@ -1,8 +1,11 @@
-// stdafx.cpp: исходный файл, содержащий только стандартные включаемые модули
-// TEST.pch будет предкомпилированным заголовком
-// stdafx.obj будет содержать предварительно откомпилированные сведения о типе
+// stdafx.cpp : source file that includes just the standard includes
+// Task1Tests.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
 
-// TODO: Установите ссылки на любые требующиеся дополнительные заголовки в файле STDAFX.H
-// , а не в данном файле
+// Сообщаем библиотеке boost test, что функция main будет предоставлена нами (для предварительного конфигурирования библиотеки), 
+// а не сгенерирована boost-ом за нас
+#define BOOST_TEST_NO_MAIN 
+// подключаем реализацию boost test
+#include <boost/test/included/unit_test.hpp>
